a175f68e016d2f505d603dc758e6ac53a94f2117
C10ver <c10ver.obj@gmail.com>
2020-05-29 01:31:35 +0600

    Fixed shitcode

--- #sourse/js/burger.js
+++ #sourse/js/burger.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let open = false;
 let btn = $('#burger');
 let menu = $('.menu');
@@ -13,20 +12,4 @@ btn.click(() => {
         menu.addClass('open');
         open = true;
     }
-=======
-let open = false;
-let btn = $('#burger');
-let menu = $('.menu');
-
-btn.click(() => {
-    if(open){
-        btn.removeClass('open');
-        menu.removeClass('open');
-        open = false;
-    } else {
-        btn.addClass('open');
-        menu.addClass('open');
-        open = true;
-    }
->>>>>>> 87a010a2c8bb8a99664922be9b5e7a52a6992d68
 });
