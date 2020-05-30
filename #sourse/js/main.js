1580bc8ebe8b3c0d4bb18ac3aa24af04fd8c4ee3
#maripbekoff <#maripbekoff@gmail.com>
2020-05-31 04:55:49 +0600

    Optimization fixes

--- #sourse/js/main.js
+++ #sourse/js/main.js
@@ -3,4 +3,6 @@ $(document).ready(() => {
     // @prepros-append script.js
     // @prepros-append burger.js
     // @prepros-append slick.js
+
+    document.addEventListener('touchstart', ontouchstart, { passive: true });
 });
