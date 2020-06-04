5b66733d5e0daf9f7e3308b68b85ccf4e355e383
#maripbekoff <#maripbekoff@gmail.com>
2020-06-04 21:03:56 +0600

    no changes

--- #sourse/js/burger.js
+++ #sourse/js/burger.js
@@ -2,26 +2,28 @@ let opened = false;
 let btn = $('#burger');
 let menu = $('.menu');
 let items = $('.menu__items');
+let nav_btn = $('.navbar__btn');
 
 
-function open(){
+function open() {
     btn.addClass('open');
     menu.addClass('open');
+    nav_btn.addClass('open');
     opened = true;
 }
 
-function close(){
+function close() {
     btn.removeClass('open');
     menu.removeClass('open');
+    nav_btn.removeClass('open');
     opened = false;
 }
 
 btn.click(() => {
-    if (opened) close(); 
+    if (opened) close();
     else open();
 });
 
 items.click(() => {
     close();
 });
-
